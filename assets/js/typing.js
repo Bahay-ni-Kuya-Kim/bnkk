a65ebd5bbb52143802e6f6e963f013c54e2d46b8
mharcnyvhielguillermo@iskolarngbayan.pup.edu.ph <mharcnyvhielguillermo@iskolarngbayan.pup.edu.ph>
2022-10-31 15:46:00 +0800

    Polished index and announcement page

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -1,33 +1,21 @@
 const txt = document.querySelector("#typing");
+const { Typed } = window;
 
-window.addEventListener("pageshow", async () => {
-    let words = ["Kagaguhan.", 
-                "Sanctuary.",
-                "Playroom.",
-                "Dogshowan.",
-                "Pogi si james."];
-    let count = 1;
-    let lpms = 200;
-    typingWords(words[0], txt, lpms);
-    setInterval(async () => {
-        if (txt.innerHTML.includes("undefined")) count = 0;
-        txt.innerHTML = "";
-        typingWords(words[count], txt, lpms);
-        if (count + 1 === words.length) {
-            count = 0;
-        } else {
-            count++;
-        }
-    }, (lpms * 2) * words[count].length)
-})
-
-function typingWords(string, elementObj, speed = 100) {
-    let arr = [...string];
-    let index = 0;
-    
-    const interval = setInterval(async () => {
-        elementObj.innerHTML += arr[index];
-        index++;
-        if (elementObj.innerHTML.length === arr.length) clearInterval(interval);
-    }, speed);
-}
+    let words = ["bagtit",
+                "gago", 
+                "naz, yawn",
+                "valo tayo",
+                "tite",
+                "pogi si james",
+                "ukinnam"];
+    const typing = new Typed("#typing", {
+      strings: words,
+      typeSpeed: 90,
+      backSpeed: 70,
+      smartBackspace: true,
+      loop: true,
+      shuffle: true,
+      backDelay: 2000,
+      startDelay: 3000,
+      cursorChar: '█'
+    });
