76725708e2fb473da557284d344715cd3fb2dc88
mharcnyvhielguillermo@iskolarngbayan.pup.edu.ph <mharcnyvhielguillermo@iskolarngbayan.pup.edu.ph>
2022-10-31 08:43:29 +0800

    Added loading screen in announcement page

--- assets/js/announcement.js
+++ assets/js/announcement.js
@@ -1,13 +1,20 @@
 const dates = document.querySelectorAll(".card-subtitle");
 const txts = document.querySelectorAll(".card-text");
+const loading = document.querySelector("#annClose");
+const ann = document.querySelector("#annOpen");
+const text = 
 
 window.addEventListener("pageshow", async () => {
+    setTimeout(() => {
+      loading.style.display = "none";
+      ann.style.display = "block";
+    }, 3000);
+    
     const raw = await fetch("https://naynaypi.is-not-a.dev/api/announcements");
     if (!raw.ok) document.location.reload();
     const infoArr = await raw.json();
     let index1 = 0;
     let index2 = 0;
-    console.log(infoArr[0])
     
     dates.forEach(date => {
         if (index1 > 4) return;
